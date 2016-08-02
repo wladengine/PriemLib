a548758b5b302541b57a68a33d9021b28a72440c
Mikelia <cmska910@gmail.com>
2016-08-02 12:19:05 +0300

    markstohist

---
+++ PriemLib/Crypto/ExamsVedMarkToHistory.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace PriemLib
+{
+    partial class ExamsVedMarkToHistory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbFaculty = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbExam = new System.Windows.Forms.ComboBox();
+            this.btnUpdateMarks = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbExamVed = new System.Windows.Forms.ComboBox();
+            this.lblPersonCount = new System.Windows.Forms.Label();
+            this.cbStudyLevelGroup = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbStudyBasis = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbmarktype = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Факультет";
+            // 
+            // cbFaculty
+            // 
+            this.cbFaculty.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFaculty.FormattingEnabled = true;
+            this.cbFaculty.Location = new System.Drawing.Point(80, 50);
+            this.cbFaculty.Name = "cbFaculty";
+            this.cbFaculty.Size = new System.Drawing.Size(185, 21);
+            this.cbFaculty.TabIndex = 1;
+            this.cbFaculty.SelectedIndexChanged += new System.EventHandler(this.cbFaculty_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Экзамен";
+            // 
+            // cbExam
+            // 
+            this.cbExam.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbExam.FormattingEnabled = true;
+            this.cbExam.Location = new System.Drawing.Point(80, 77);
+            this.cbExam.Name = "cbExam";
+            this.cbExam.Size = new System.Drawing.Size(445, 21);
+            this.cbExam.TabIndex = 1;
+            this.cbExam.SelectedIndexChanged += new System.EventHandler(this.cbExam_SelectedIndexChanged);
+            // 
+            // btnUpdateMarks
+            // 
+            this.btnUpdateMarks.Location = new System.Drawing.Point(312, 211);
+            this.btnUpdateMarks.Name = "btnUpdateMarks";
+            this.btnUpdateMarks.Size = new System.Drawing.Size(175, 23);
+            this.btnUpdateMarks.TabIndex = 2;
+            this.btnUpdateMarks.Text = "Перенести результаты";
+            this.btnUpdateMarks.UseVisualStyleBackColor = true;
+            this.btnUpdateMarks.Click += new System.EventHandler(this.btnUpdateMarks_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Ведомость";
+            // 
+            // cbExamVed
+            // 
+            this.cbExamVed.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbExamVed.FormattingEnabled = true;
+            this.cbExamVed.Location = new System.Drawing.Point(80, 104);
+            this.cbExamVed.Name = "cbExamVed";
+            this.cbExamVed.Size = new System.Drawing.Size(445, 21);
+            this.cbExamVed.TabIndex = 1;
+            this.cbExamVed.SelectedIndexChanged += new System.EventHandler(this.cbExamVed_SelectedIndexChanged);
+            // 
+            // lblPersonCount
+            // 
+            this.lblPersonCount.Location = new System.Drawing.Point(80, 169);
+            this.lblPersonCount.Name = "lblPersonCount";
+            this.lblPersonCount.Size = new System.Drawing.Size(445, 39);
+            this.lblPersonCount.TabIndex = 3;
+            this.lblPersonCount.Text = " ";
+            // 
+            // cbStudyLevelGroup
+            // 
+            this.cbStudyLevelGroup.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbStudyLevelGroup.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbStudyLevelGroup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbStudyLevelGroup.DropDownWidth = 350;
+            this.cbStudyLevelGroup.FormattingEnabled = true;
+            this.cbStudyLevelGroup.Location = new System.Drawing.Point(80, 9);
+            this.cbStudyLevelGroup.Name = "cbStudyLevelGroup";
+            this.cbStudyLevelGroup.Size = new System.Drawing.Size(445, 21);
+            this.cbStudyLevelGroup.TabIndex = 149;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 148;
+            this.label4.Text = "Уровень";
+            // 
+            // cbStudyBasis
+            // 
+            this.cbStudyBasis.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbStudyBasis.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbStudyBasis.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbStudyBasis.FormattingEnabled = true;
+            this.cbStudyBasis.Location = new System.Drawing.Point(389, 50);
+            this.cbStudyBasis.Name = "cbStudyBasis";
+            this.cbStudyBasis.Size = new System.Drawing.Size(136, 21);
+            this.cbStudyBasis.TabIndex = 151;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(289, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 150;
+            this.label5.Text = "Основа обучения";
+            // 
+            // lbmarktype
+            // 
+            this.lbmarktype.FormattingEnabled = true;
+            this.lbmarktype.Location = new System.Drawing.Point(534, 35);
+            this.lbmarktype.Name = "lbmarktype";
+            this.lbmarktype.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lbmarktype.Size = new System.Drawing.Size(176, 199);
+            this.lbmarktype.TabIndex = 152;
+            this.lbmarktype.SelectedIndexChanged += new System.EventHandler(this.lbmarktype_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(531, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 13);
+            this.label6.TabIndex = 153;
+            this.label6.Text = "Категории оценок";
+            // 
+            // ExamsVedMarkToHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 255);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbmarktype);
+            this.Controls.Add(this.cbStudyBasis);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbStudyLevelGroup);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblPersonCount);
+            this.Controls.Add(this.btnUpdateMarks);
+            this.Controls.Add(this.cbExamVed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbExam);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbFaculty);
+            this.Controls.Add(this.label1);
+            this.Name = "ExamsVedMarkToHistory";
+            this.Text = "Перенос оценок ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbFaculty;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbExam;
+        private System.Windows.Forms.Button btnUpdateMarks;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbExamVed;
+        private System.Windows.Forms.Label lblPersonCount;
+        public System.Windows.Forms.ComboBox cbStudyLevelGroup;
+        protected System.Windows.Forms.Label label4;
+        protected System.Windows.Forms.ComboBox cbStudyBasis;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox lbmarktype;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ PriemLib/Crypto/ExamsVedMarkToHistory.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using BDClassLib;
+using EducServLib;
+using BaseFormsLib;
+
+namespace PriemLib
+{
+    public partial class ExamsVedMarkToHistory : Form
+    {
+        public int? StudyLevelGroupId
+        {
+            get { return ComboServ.GetComboIdInt(cbStudyLevelGroup); }
+            set { ComboServ.SetComboId(cbStudyLevelGroup, value); }
+        }
+        public int? FacultyId
+        {
+            get { return ComboServ.GetComboIdInt(cbFaculty); }
+            set { ComboServ.SetComboId(cbFaculty, value); }
+        }
+        public int? StudyBasisId
+        {
+            get { return ComboServ.GetComboIdInt(cbStudyBasis); }
+            set { ComboServ.SetComboId(cbStudyBasis, value); }
+        }
+        public int? ExamId
+        {
+            get { return ComboServ.GetComboIdInt(cbExam); }
+            set { ComboServ.SetComboId(cbExam, value); }
+        }
+        public List<Guid> gExamsVed;
+
+        public ExamsVedMarkToHistory()
+        {
+            InitializeComponent();
+            SelectedMarkTypes = new List<int>();
+            this.MdiParent = MainClass.mainform;
+            FillFaculty();
+        }
+        protected virtual List<KeyValuePair<string, string>> GetSourceStudyLevelGroup()
+        {
+            using (PriemEntities context = new PriemEntities())
+            {
+                var src = MainClass.GetEntry(context)
+                    .Select(x => new { x.StudyLevelGroupId, x.StudyLevelGroupName })
+                    .Distinct()
+                    .ToList()
+                    .Select(x => new KeyValuePair<string, string>(x.StudyLevelGroupId.ToString(), x.StudyLevelGroupName))
+                    .ToList();
+
+                return src;
+            }
+        }
+        public void FillFaculty()
+        {
+            FillMarkType();
+            ComboServ.FillCombo(cbStudyLevelGroup, GetSourceStudyLevelGroup(), false, false);
+            ComboServ.FillCombo(cbFaculty, HelpClass.GetComboListByTable("ed.qFaculty", "ORDER BY Acronym"), false, true);
+            ComboServ.FillCombo(cbStudyBasis, HelpClass.GetComboListByTable("ed.StudyBasis", "ORDER BY Name"), false, true);
+            
+        }
+        public void FillMarkType()
+        {
+            using (PriemEntities context = new PriemEntities())
+            {
+                var lst = (from x in context.ExamsVedMarkType
+                               select new {
+                                   x.Id, x.Name
+                               }).ToList().OrderBy(x=>x.Id).Select(u => new KeyValuePair<string, string>(
+                          u.Id.ToString(), u.Name)).ToList();
+                lbmarktype.DataSource = lst;
+                lbmarktype.ValueMember = "Key";
+                lbmarktype.DisplayMember = "Value";
+                lbmarktype.ClearSelected();
+
+                lbmarktype.SelectedIndices.Add(2);
+                lbmarktype.SelectedIndices.Add(3);
+                lbmarktype.SelectedIndices.Add(4);
+                lbmarktype.SelectedIndices.Add(5);
+            }
+        }
+        public void FillExam()
+        {
+            using (PriemEntities context = new PriemEntities())
+            {
+                List<Guid> veds = (from x in context.ExamsVedSelectedMarkType
+                                   where SelectedMarkTypes.Contains(x.MarkTypeId)
+                                   select x.ExamsVedId).Distinct().ToList();
+
+                List<KeyValuePair<string, string>> lst =
+                    ((from ent in context.extExamsVed
+                      
+                      where MainClass.lstStudyLevelGroupId.Contains(ent.StudyLevelGroupId)
+                      && (FacultyId.HasValue ?  ent.FacultyId == FacultyId : true)
+                      && (StudyBasisId != null ? ent.StudyBasisId == StudyBasisId : true == true)
+                      && (veds.Count > 0 ? veds.Contains(ent.Id) : true)
+                      select new
+                      {
+                          Id = ent.ExamId,
+                          Name = ent.ExamName,
+                      }).Distinct()).ToList().OrderBy(x => x.Name)
+                      .Select(u => new KeyValuePair<string, string>(
+                          u.Id.ToString(), u.Name)).ToList();
+                ComboServ.FillCombo(cbExam, lst, false, true);
+            }
+        }
+        public void FillVed()
+        {
+            using (PriemEntities context = new PriemEntities())
+            {
+
+                List<Guid> veds = (from x in context.ExamsVedSelectedMarkType
+                                   where SelectedMarkTypes.Contains(x.MarkTypeId)
+                                   select x.ExamsVedId).Distinct().ToList();
+
+
+                List<KeyValuePair<string, string>> lst =
+                    ((from ent in context.extExamsVed
+                      where MainClass.lstStudyLevelGroupId.Contains(ent.StudyLevelGroupId)
+                       && (FacultyId.HasValue ? ent.FacultyId == FacultyId : true)
+                      && (StudyBasisId != null ? ent.StudyBasisId == StudyBasisId : true == true)
+                      && ((ExamId  != null) ? ent.ExamId == ExamId : true)
+                      && (veds.Count > 0 ? veds.Contains(ent.Id) : true)
+                      select new
+                      {
+                          ent.Id,
+                          ent.Number,
+                          ent.ExamName,
+                          ent.Date,
+                          StBasis = ent.StudyBasisId == null ? "" : ent.StudyBasisAcr,
+                          AddVed = ent.IsAddVed ? " дополнительная" : "",
+                          ent.AddCount
+                      }).Distinct()).ToList().OrderBy(x => x.Date).ThenBy(x => x.ExamName).ThenBy(x => x.Number)
+                      .Select(u => new KeyValuePair<string, string>(
+                          u.Id.ToString(),
+                          "[" + u.Number + "] " + u.ExamName + ' ' + u.Date.ToShortDateString() + ' ' + u.StBasis + u.AddVed +
+                            (u.AddCount > 1 ? "(" + Convert.ToString(u.AddCount) + ")" : ""))).ToList();
+
+                ComboServ.FillCombo(cbExamVed, lst, false, true);
+            }
+        }
+
+        private void cbFaculty_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillExam();
+        }
+
+        private void cbExam_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillVed();
+        }
+
+        private void cbExamVed_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (PriemEntities context = new PriemEntities())
+            {
+                Guid? VedId =  ComboServ.GetComboIdGuid(cbExamVed);
+                List<Guid> veds = (from x in context.ExamsVedSelectedMarkType
+                                   where SelectedMarkTypes.Contains(x.MarkTypeId)
+                                   select x.ExamsVedId).Distinct().ToList();
+                gExamsVed =  
+                    (from ent in context.extExamsVed
+                      where MainClass.lstStudyLevelGroupId.Contains(ent.StudyLevelGroupId)
+                       && (FacultyId.HasValue ? ent.FacultyId == FacultyId : true)
+                      && (StudyBasisId != null ? ent.StudyBasisId == StudyBasisId : true == true)
+                      && ((ExamId  != null) ? ent.ExamId == ExamId : true)
+                      && (VedId.HasValue ? ent.Id == VedId.Value : true)
+                      && (veds.Count > 0 ? veds.Contains(ent.Id) : true)
+                      select ent.Id).Distinct().ToList();
+
+                var Cnt = (from x in context.ExamsVedHistory 
+                               where gExamsVed.Contains(x.ExamsVedId)
+                               select x.Id).Count();
+
+                lblPersonCount.Text = gExamsVed.Count().ToString() + " ведомостей // " + Cnt.ToString() + " записей в ведомостях";
+            }
+        }
+        List<int> SelectedMarkTypes;
+        private void lbmarktype_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectedMarkTypes = new List<int>();
+            foreach (KeyValuePair<string,string> x in lbmarktype.SelectedItems)
+            {
+                SelectedMarkTypes.Add(int.Parse(x.Key));
+            }
+            FillExam();
+        }
+
+        private void btnUpdateMarks_Click(object sender, EventArgs e)
+        {
+            using (PriemEntities context = new PriemEntities())
+            {
+                foreach (Guid VedId in gExamsVed)
+                {
+                    // если MarkValue == null ???? 
+                    var Marks = (from x in context.ExamsVedHistoryMark
+                                 join h in context.ExamsVedHistory on x.ExamsVedHistoryId equals h.Id
+                                 where h.ExamsVedId == VedId
+                                 select x).ToList().GroupBy(x=>x.ExamsVedHistoryId).Select(x=> new 
+                                 {
+                                    ExamsVedHistoryId = x.Key,
+                                    MarkSum = x.Sum(t=>t.MarkValue??0),
+                                 }).ToList();
+                    foreach (var Mark in Marks)
+                    {
+                        ExamsVedHistory H = context.ExamsVedHistory.Where(x => x.Id == Mark.ExamsVedHistoryId).First();
+                        H.Mark = Mark.MarkSum;
+                        context.SaveChanges();
+                    }
+                }
+            }
+        }
+    }
+}
